4a53ffb2fcb42fabfdb919201d875cc978a8c1c5
Michael Rozumyanskiy <mr@pixty.com>
2019-04-01 09:20:23 +0300

    Bump Gradle version to 5.3.1

--- pablo/src/main/java/io/michaelrocks/pablo/DependencyResolver.kt
+++ pablo/src/main/java/io/michaelrocks/pablo/DependencyResolver.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018 Michael Rozumyanskiy
+ * Copyright 2019 Michael Rozumyanskiy
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -97,8 +97,7 @@ class DependencyResolver private constructor(
   }
 
   private fun resolve(dependencyConstraint: DependencyConstraint, scope: Scope) {
-    val notation = DependencyNotation(dependencyConstraint.group, dependencyConstraint.name,
-        dependencyConstraint.version)
+    val notation = DependencyNotation(dependencyConstraint.group, dependencyConstraint.name, dependencyConstraint.version ?: "")
     builder.addDependencyNotation(scope, notation)
   }
 
